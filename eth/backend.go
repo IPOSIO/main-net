d140123d6b6dc125f6fd0e3e4b441f2fa25a5863
dappstore123 <>
2019-04-10 16:48:22 +0800

    gipos

--- eth/backend.go
+++ eth/backend.go
@@ -212,7 +212,7 @@ func makeExtraData(extra []byte) []byte {
 		// create default extradata
 		extra, _ = rlp.EncodeToBytes([]interface{}{
 			uint(params.VersionMajor<<16 | params.VersionMinor<<8 | params.VersionPatch),
-			"geth",
+			"gipos",
 			runtime.Version(),
 			runtime.GOOS,
 		})
